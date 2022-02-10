a5baee0fa69112c52b785beed9c89df766f4c519
jjam123007 <65060360+jjam123007@users.noreply.github.com>
2022-02-10 19:20:47 +1000

    first commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://jjam123007:jjam007123@cluster0.upru5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then( () => app.listen(5000, console.log(`Server running on port ${PORT}`)))
+.catch( (e) => console.log(e.message));
+
+app.use('/posts', postRoutes);
